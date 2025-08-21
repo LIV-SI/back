1d242e7f2d68384fa1acaa5ec834a4fdb4d5f568
david3478 <inwookim03@naver.com>
2025-08-21 18:00:06 +0900

    Init

---
+++ VideoAnalyze2/src/main/java/hello/videoanalyze2/SceneData.java
@@ -0,0 +1,10 @@
+package hello.videoanalyze2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class SceneData {
+    private String text;
+    private String BGVN;
+}
---
+++ VideoAnalyze2/src/main/java/hello/videoanalyze2/VideoGenerationService.java
@@ -0,0 +1,11 @@
+package hello.videoanalyze2;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class VideoGenerationService {
+    public void generateMultiSceneVideo(String sigunguEnglish, String voicePack, List<SceneData> sceneDataList) {
+    }
+}
