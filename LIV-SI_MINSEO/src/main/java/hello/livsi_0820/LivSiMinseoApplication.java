38d8bedea0981c5782bd136101f462fe91d6d478
OpAue <kkt23917@naver.com>
2025-08-25 02:27:24 +0900

    Swagger fix

--- LIV-SI_MINSEO/src/main/java/hello/livsi_0820/LivSiMinseoApplication.java
+++ LIV-SI_MINSEO/src/main/java/hello/livsi_0820/LivSiMinseoApplication.java
@@ -1,11 +1,14 @@
 package hello.livsi_0820;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class LivSiMinseoApplication {
 
 	public static void main(String[] args) {
